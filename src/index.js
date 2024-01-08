53923085a5c127f031ff0d7071a3c8d55926d20e
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-01-08 19:50:30 +0000

    see how gravity keeps increasing velocity

--- src/index.js
+++ src/index.js
@@ -4,11 +4,15 @@ const config = {
   width: 800,
   height: 600,
   physics: {
-    default: "arcade"
+    default: "arcade",
+    arcade: {
+      gravity: { y: 50 }
+    }
   },
   scene: {
     preload,
-    create
+    create,
+    update
   }
 };
 
@@ -16,11 +20,22 @@ function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
 }
+
 let bird = null;
 function create() {
   //this.add.image(config.width / 2, config.height / 2, 'sky');
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  bird = this.add.sprite(config.width / 10, config.height / 2, 'bird').setOrigin(0);
-  debugger;
+  bird = this.physics.add.sprite(config.width / 10, config.height / 10, 'bird').setOrigin(0);
+  // bird.body.gravity.y = 50;
+  bird.body.velocity.x = 50;
+}
+
+let totalDelta = 0;
+function update(time, delta) {
+  if (totalDelta >= 1000) {
+    console.log(bird.body.velocity.y);
+    totalDelta = 0;
+  }
+  totalDelta += delta;
 }
 new Phaser.Game(config);
