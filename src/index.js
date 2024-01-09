2e7e57bce3ea049bfe7873a806b2a158aef7dbc3
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-01-09 11:08:08 +0000

    random size of vertical gap between pipes

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ const config = {
     default: "arcade",
     arcade: {
       debug: true,
-      gravity: { y: 10 }
+      // gravity: { y: 10 }
     }
   },
   scene: {
@@ -17,24 +17,34 @@ const config = {
   }
 };
 
+// background has 40-pixel wide banding
 let bird = null;
-const SPEED = 200;
+let pipes = [];
+const xSpeed = 100;
 const initBirdPos = { x: config.width / 10, y: config.height / 4 };
+const pipeX = 300;
+const gapTop = 200;
+const yGapMin = 150;
+const yGapMax = 250;
+let yGap = Phaser.Math.Between(yGapMin, yGapMax);
+const pipeHeight = 480;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
   this.load.image('bird', 'assets/bird.png');
-  this.load.image('pipe', 'aasets/pipe.png');
-}
+  this.load.image('pipe', 'assets/pipe.png');
+};
 
 function create() {
   //this.add.image(config.width / 2, config.height / 2, 'sky');
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
-  this.add.image(200, 600, 'pipe');
 
+  pipes[0] = this.physics.add.sprite(pipeX, gapTop, 'pipe').setOrigin(0, 1);
+  pipes[1] = this.physics.add.sprite(pipeX, gapTop + yGap, 'pipe').setOrigin(0, 0);
+  console.log(pipes);
   bird = this.physics.add.sprite(initBirdPos.x, initBirdPos.y, 'bird');
-  // bird.body.gravity.y = 50;
-  bird.body.velocity.x = SPEED;
+  bird.body.gravity.y = 40;
+  bird.body.velocity.x = xSpeed;
   console.log(bird);
   this.input.on('pointerdown', flap);
   let spacebar = this.input.keyboard.addKey('SPACE');
@@ -63,11 +73,11 @@ function update(time, delta) {
 function restartBirdPosition() {
   bird.x = initBirdPos.x;
   bird.y = initBirdPos.y;
-  bird.body.velocity.x = SPEED;
+  bird.body.velocity.x = xSpeed;
   bird.body.velocity.y = 0;
 }
 function flap() {
-  bird.body.velocity.y = -SPEED / 2;
+  bird.body.velocity.y = -xSpeed / 2;
   console.log("flapping");
 }
 new Phaser.Game(config);
