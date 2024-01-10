c4036733455e84152fba67eceffae1932138e3c6
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-01-10 08:55:23 +0000

    recycle pipes, and range list exapansions

--- src/index.js
+++ src/index.js
@@ -28,19 +28,18 @@ const xSpeed = 0;
 const initBirdX = 50;
 const initBirdY = 50; //config.height / 10;
 
-// these will both vary with difficulty
-const yGapMin = 150;
-const yGapMax = 250;
-let yGap = Phaser.Math.Between(yGapMin, yGapMax);
-
+// to avoid gap above upper pipe, gapTopMax cannot exceed pipeGraphicHeight
 const pipeGraphicHeight = 480;
 const pipeHeightMinimum = 10;
-const gapTopMin = pipeHeightMinimum;
-const gapTopMax = config.height - yGap - pipeHeightMinimum;
-// to avoid visual error of gap at screentop, gapTopMax cannot exceed pipeGraphicHeight
 
-let pipeX = 500;  //Phaser.Math.Between(100, 700);
-let pipeSpacingX = 800;
+// these can vary with difficulty
+let xFirstPipeRange = [400, 600];
+let pipeX = Phaser.Math.Between(...xFirstPipeRange);
+let xSpacingRange = [400, 600];
+let yGapRange = [150, 250];
+
+// let xSpacing = null;
+// let gapTop = null;
 
 function preload() {
   this.load.image('sky', 'assets/sky.png');
@@ -51,29 +50,21 @@ function preload() {
 function create() {
   //this.add.image(config.width / 2, config.height / 2, 'sky');
   this.add.image(0, 0, 'sky').setOrigin(0, 0);
+  pipes = this.physics.add.group();
 
   for (let i = 0; i < PIPE_NUM; i++) {
-    let upperPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 1);
-    let lowerPipe = this.physics.add.sprite(0, 0, 'pipe').setOrigin(0, 0);
-    placePipe(upperPipe, lowerPipe, pipeX);
-
-    // let gapTop = Phaser.Math.Between(gapTopMin, gapTopMax);
-    // pipes[0] = this.physics.add.sprite(pipeX, gapTop, 'pipe').setOrigin(0, 1);
-    // pipes[1] = this.physics.add.sprite(pipeX, gapTop + yGap, 'pipe').setOrigin(0, 0);
-    // pipes[0].body.velocity.x = -100;
-    // pipes[1].body.velocity.x = -100;
-    // console.log(pipes);
-    // pipeX += pipeSpacingX;
+    let upperPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 1);
+    let lowerPipe = pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+    placePipe(upperPipe, lowerPipe);
   }
+  pipes.setVelocityX(-100);
 
-  bird = this.physics.add.sprite(initBirdX, initBirdY, 'bird');  //(initBirdPos.x, initBirdPos.y, 'bird');
+  bird = this.physics.add.sprite(initBirdX, initBirdY, 'bird');
   bird.body.gravity.y = gravity;
   bird.body.velocity.x = 0;
-  console.log(bird);
   this.input.on('pointerdown', flap);
   let spacebar = this.input.keyboard.addKey('SPACE');
   spacebar.on('down', flap);
-  //this.input.keyboard.on('keydown_SPACE', flap);
 }
 
 function update(time, delta) {
@@ -94,21 +85,31 @@ function update(time, delta) {
   }
 }
 
-function redoPipes() {
-  yGap = Phaser.Math.Between(yGapMin, yGapMax);
-  gapTop = Phaser.Math.Between(gapTopMin, gapTopMax);
-  pipeX = Phaser.Math.Between(100, 700);
-}
-
 function placePipe(uPipe, lPipe) {
-  let gapTop = Phaser.Math.Between(gapTopMin, gapTopMax);
+  let yGap = Phaser.Math.Between(...yGapRange);
+  let gapTopMax = config.height - yGap - pipeHeightMinimum;
+  let gapTopRange = [pipeHeightMinimum, gapTopMax];
+  let gapTop = Phaser.Math.Between(...gapTopRange);
+  let xSpacing = Phaser.Math.Between(...xSpacingRange);
+  let rightX = getRightMostPipe();
+  console.log(xSpacing, gapTop, pipeX, rightX);
   uPipe.x = pipeX;
   uPipe.y = gapTop;
   lPipe.x = pipeX;
   lPipe.y = gapTop + yGap;
-  uPipe.body.velocity.x = -100;
-  lPipe.body.velocity.x = -100;
-  pipeX += pipeSpacingX;
+  pipeX += xSpacing;
+}
+
+function recyclePipes() {
+  let tempPipes = [];
+  pipes.getChildren().forEach(pipe => {
+    if (pipe.getBounds().right < 0) {
+      tempPipes.push(pipe);
+      if (tempPipes.length === 2) {
+        placePipe(...tempPipes);
+      }
+    }
+  });
 }
 
 function restartBirdPosition() {
@@ -118,8 +119,17 @@ function restartBirdPosition() {
   bird.body.velocity.y = 0;
 }
 
+function getRightMostPipe() {
+  let rightmostX = 0;
+  pipes.getChildren().forEach(pipe => {
+    rightmostX = Math.max(pipe.x, rightmostX);
+  });
+  return rightmostX;
+}
+
 function flap() {
   bird.body.velocity.y -= flapSpeed;
   console.log("flapping");
 }
+
 new Phaser.Game(config);
