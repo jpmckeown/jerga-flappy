bc6096926206275bb986e50e5d8c09211a4115ad
J Patrick McKeown <jpmckeown@users.noreply.github.com>
2024-01-10 20:10:52 +0000

    restart game when collide pipe or ground or ceiling

--- src/scenes/play.js
+++ src/scenes/play.js
@@ -6,7 +6,7 @@ class Play extends Phaser.Scene {
     super('Play');
     this.config = config;
     this.bird = null;
-    this.flapSpeed = 50;
+    this.boostVelocity = 70;
     this.pipes = null;
     this.xSpacingRange = [400, 600];
     this.yGapRange = [150, 250];
@@ -35,8 +35,12 @@ class Play extends Phaser.Scene {
   makePipes() {
     this.pipes = this.physics.add.group();
     for (let i = 0; i < PIPES_TO_RENDER; i++) {
-      let upperPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 1);
-      let lowerPipe = this.pipes.create(0, 0, 'pipe').setOrigin(0, 0);
+      let upperPipe = this.pipes.create(0, 0, 'pipe')
+        .setImmovable(true)
+        .setOrigin(0, 1);
+      let lowerPipe = this.pipes.create(0, 0, 'pipe')
+        .setImmovable(true)
+        .setOrigin(0, 0);
       this.placePipe(upperPipe, lowerPipe);
     }
     this.pipes.setVelocityX(-100);
@@ -44,18 +48,19 @@ class Play extends Phaser.Scene {
 
   makeBird() {
     this.bird = this.physics.add.sprite(this.config.playerInitX, this.config.playerInitY, 'bird');
-    this.bird.body.gravity.y = 30;
+    this.bird.body.gravity.y = 50;
     this.bird.body.velocity.x = 0;
+    // this.bird.setCollideWorldBounds(true);
   }
 
   makeColliders() {
-    this.physics.add.collider(this.bird, this.pipes);
+    this.physics.add.collider(this.bird, this.pipes, this.gameOver, null, this);
   }
 
   handleInputs() {
-    this.input.on('pointerdown', this.flap, this);
+    this.input.on('pointerdown', this.boost, this);
     let spacebar = this.input.keyboard.addKey('SPACE');
-    spacebar.on('down', this.flap, this);
+    spacebar.on('down', this.boost, this);
   }
 
   placePipe(uPipe, lPipe) {
@@ -94,31 +99,39 @@ class Play extends Phaser.Scene {
   }
 
   checkBirdCrash() {
-    if (this.bird.body.position.x > this.config.width - this.bird.body.width) {
-      this.bird.body.velocity.x *= -1;
-    }
-    else if (this.bird.body.position.x < 0) {
-      this.bird.body.velocity.x *= -1;
-    }
-    // allow bird to go above top of screen
-    if (this.bird.body.position.y > this.config.height - this.bird.height) {
+    if (this.bird.getBounds().bottom > this.config.height) {
       console.log("You crashed into the ground.");
-      this.restartBirdPosition();
+      this.gameOver();
     }
-    else if (this.bird.body.position.y < -240) {
+    else if (this.bird.getBounds().top < 0) {
       console.log("You flew too high near the sun.");
-      this.restartBirdPosition();
+      this.gameOver();
     }
+    // if (this.bird.body.position.x > this.config.width - this.bird.body.width) {
+    //   this.bird.body.velocity.x *= -1;
+    // }
+    // else if (this.bird.body.position.x < 0) {
+    //   this.bird.body.velocity.x *= -1;
+    // }
   }
 
-  restartBirdPosition() {
-    this.bird.x = this.config.playerInitX;
-    this.bird.y = this.config.playerInitY;
-    this.bird.body.velocity.y = 0;
+  gameOver() {
+    this.physics.pause();
+    this.bird.setTint(0xff0000);
+    this.time.addEvent({
+      delay: 1000,
+      callback: () => {
+        this.scene.restart();
+      },
+      loop: false
+    });
+    // this.bird.x = this.config.playerInitX;
+    // this.bird.y = this.config.playerInitY;
+    // this.bird.body.velocity.y = 0;
   }
 
-  flap() {
-    this.bird.body.velocity.y -= this.flapSpeed;
+  boost() {
+    this.bird.body.velocity.y -= this.boostVelocity;
     console.log("flapping");
   }
 }
